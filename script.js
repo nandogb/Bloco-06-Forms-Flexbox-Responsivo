ff3f3e2103d495b0e03b3baec869995f45e0f72c
Fernando Barreto <fernando.gbarreto@hotmail.com>
2022-08-04 13:56:50 -0300

    dia 01 e exercicios

---
+++ script.js
@@ -0,0 +1,10 @@
+// SELETORES
+// const INPUT_TEXT = document.querySelector("#input-text");
+// const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const sumbmitForm = document.querySelector("#enviar");
+
+function enviarForm(event) {
+  event.preventDefault()
+}
+
+sumbmitForm.addEventListener("click", enviarForm);
