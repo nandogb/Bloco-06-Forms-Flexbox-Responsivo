98ef58579e8630e49f72d8e4b782372c269157ce
Fernando Barreto <fernando.gbarreto@hotmail.com>
2022-08-08 11:15:36 -0300

    dia 02

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // SELETORES
 // const INPUT_TEXT = document.querySelector("#input-text");
 // const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
-const sumbmitForm = document.querySelector("#enviar");
+// const sumbmitForm = document.querySelector("#enviar");
 
-function enviarForm(event) {
-  event.preventDefault()
+// function enviarForm(event) {
+//   event.preventDefault()
+// }
+
+// sumbmitForm.addEventListener("click", enviarForm);
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.datepicker');
+  var instances = M.Datepicker.init(elems, options);
 }
-
-sumbmitForm.addEventListener("click", enviarForm);
